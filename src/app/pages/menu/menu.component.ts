75a383448d3ea639f931172526a87592935deb43
S-Geetha <geethas.ug.21.cs@francisxavier.ac.in>
2023-09-26 22:17:22 +0530

    first commit

---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private service:OrderDetailsService) { }
+  Data:any;
+  ngOnInit(): void {
+    this.Data = this.service.Details;
+  }
+
+}
---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,127 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+
+  
+  Details = [
+    
+    {
+      id:1,
+      Name:"Hopscotch",
+      Details:"Baby Girl's Fit & Flare Long Dress",
+      Price:1269,
+      Img:"https://m.media-amazon.com/images/I/51hFCot9NBL._AC_UL480_FMwebp_QL65_.jpg"
+    },
+    {
+      id:2,
+      Name:"Baby Lehanga",
+      Details:"Colour : Yellow & Maroon",
+      Price:415,
+      Img:"https://i.etsystatic.com/28999595/c/2000/2000/0/0/il/fc86f3/4305392758/il_300x300.4305392758_3omq.jpg"
+    },
+   
+    {
+      id:3,
+      Name:"Maxi Dress",
+      Details:"Fashion Fly",
+      Price:299,
+      Img:"https://m.media-amazon.com/images/I/61ta8iQetuL._AC._SR360,460.jpg"
+    },
+    {
+      id:4,
+      Name:"ICABLE Disney Baby Girls Midi",
+      Details:"Cotton Blend Minnie Dress",
+      Price:350,
+      Img:"https://m.media-amazon.com/images/I/51iiWL4gI+L._AC._SR180,230.jpg"
+    },
+    {
+      id:5,
+      Name:"FIREFINCH Frock",
+      Details:"Maroon Floral Printed",
+      Price:496,
+      Img:"https://m.media-amazon.com/images/I/51lPkRHnUML._AC._SR360,460.jpg"
+    },
+    {
+      id:6,
+      Name:"LITTLEWINGS Pari Frock",
+      Details:"Party Festive",
+      Price:399,
+      Img:"https://m.media-amazon.com/images/I/71PTS03APVL._AC_UL480_FMwebp_QL65_.jpg"
+    },
+    
+    {
+      id:7,
+      Name:"Half - Sleeve Dress",
+      Details:"Colour : Lavender",
+      Price:110,
+      Img:"https://images.meesho.com/images/products/94252478/vvcjt_512.webp"
+    },
+    {
+      id:8,
+      Name:"Hopscotch",
+      Details:"Knee Length Dress",
+      Price:405,
+      Img:"https://m.media-amazon.com/images/I/61gi6ZEh-IL._AC_UL480_FMwebp_QL65_.jpg"
+    },
+      {
+        id:9,
+        Name:"OMLI Toddler Dress",
+        Details:"Long Sleeve Infant Casual Dress",
+        Price:480,
+        Img:"https://m.media-amazon.com/images/I/517h+EN1EcL._AC_UL480_QL65_.jpg"
+      },
+      {
+        id:10,
+        Name:"BABY GO Full Sleeve Frocks",
+        Details:"Round Neck A Line Frocks for Baby Girls",
+        Price:499,
+        Img:"https://m.media-amazon.com/images/I/615pksFmAlL._AC_UL480_QL65_.jpg"
+      },
+      {
+        id:11,
+        Name:"Polycotton Animal Print",
+        Details:" Onesie In Multi Color",
+        Price:719,
+        Img:"https://m.media-amazon.com/images/I/51bDaNYX3BL._AC_UL480_QL65_.jpg"
+      },
+      {
+        id:12,
+        Name:"Fashion Dream",
+        Details:"Baby Girls Midi Frock",
+        Price:584,
+        Img:"https://m.media-amazon.com/images/I/81azZEfWvXL._AC_UL480_FMwebp_QL65_.jpg"
+      },
+      {
+        id:13,
+        Name:"Singlet Frock Floral Print",
+        Details:"Cotton Knit Full Sleeves",
+        Price:696,
+        Img:"https://cdn.fcglcdn.com/brainbees/images/products/300x364/11546550a.webp"
+      },
+      {
+        id:14,
+        Name:"Babyhug Cotton knit",
+        Details:"Three Fourth Sleeves Frock with Leggings",
+        Price:600,
+        Img:"https://cdn.fcglcdn.com/brainbees/images/products/300x364/13724675a.webp"
+      },
+      {
+        id:15,
+        Name:"Floral Embroidery",
+        Details:"Full Sleeves Top & Denim Jeans",
+        Price:961,
+        Img:"https://cdn.fcglcdn.com/brainbees/images/products/300x364/12946944a.webp"
+      }
+
+  ]
+
+
+  
+
+}
